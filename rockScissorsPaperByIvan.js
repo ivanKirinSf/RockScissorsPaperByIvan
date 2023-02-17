e8ca333429ed007d06fbcabdde9af3eb7489f5f5
Ivan Kirin <arch.ivan.kirin@gmail.com>
2023-02-16 21:33:55 -0800

    update

---
+++ rockScissorsPaperByIvan.js
@@ -0,0 +1,11 @@
+const rock = "Rock";
+const paper = "Paper";
+const scissors = "Scissors";
+
+if (playerTurn == "r"||playerTurn == "rock"){
+    playerTurn = rock;
+} else if (playerTurn == "p" || playerTurn == "paper"){
+    playerTurn = paper;
+} else if (playerTurn == "s" || playerTurn == "scissors"){
+    playerTurn = scissors;
+}
