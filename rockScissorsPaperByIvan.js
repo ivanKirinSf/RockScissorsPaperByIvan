b1621410b3d49a03071cf097395327247e061912
Ivan Kirin <arch.ivan.kirin@gmail.com>
2023-02-18 00:37:02 -0800

    Update

--- rockScissorsPaperByIvan.js
+++ rockScissorsPaperByIvan.js
@@ -1,6 +1,7 @@
 const rock = "Rock";
 const paper = "Paper";
 const scissors = "Scissors";
+let playerTurn = "";
 
 if (playerTurn == "r"||playerTurn == "rock"){
     playerTurn = rock;
@@ -8,4 +9,17 @@ if (playerTurn == "r"||playerTurn == "rock"){
     playerTurn = paper;
 } else if (playerTurn == "s" || playerTurn == "scissors"){
     playerTurn = scissors;
+} else {
+    console.log("Invalid Input. Try Again...");
+}
+
+let computerRandomNumber = Math.floor(Math.random()*3)+1;
+
+switch(computerRandomNumber){
+    case 1 : "Rock";
+    break;
+    case 2: "Paper";
+    break;
+    case 3: "Scissors";
+    break;
 }
