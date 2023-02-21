c801bfffac151f0eb75b4541283b938f266d192a
Ivan Kirin <arch.ivan.kirin@gmail.com>
2023-02-20 21:43:16 -0800

    update

--- rockScissorsPaperByIvan.js
+++ rockScissorsPaperByIvan.js
@@ -1,7 +1,8 @@
 const rock = "Rock";
 const paper = "Paper";
 const scissors = "Scissors";
-let playerTurn = "";
+let playerTurn = "scissors";
+
 
 if (playerTurn == "r"||playerTurn == "rock"){
     playerTurn = rock;
@@ -30,5 +31,14 @@ switch(computerRandomNumber){
 console.log(`The computer chooses ${computerTurn}`);
 
 if((playerTurn === rock & computerTurn === scissors) || (playerTurn === paper & computerTurn === rock) || (playerTurn === scissors & computerTurn === paper) ){
+    
     console.log("You win!");
+}else if((playerTurn === rock & computerTurn === paper) || (playerTurn === paper & computerTurn === scissors) || (playerTurn === scissors & computerTurn === rock) ){
+    
+    console.log("You lose!");
+}else {
+    console.log("This game was a draw!");
 }
+
+
+
